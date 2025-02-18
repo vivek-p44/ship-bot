9950ec802342881e2cddb75fd94fcbc4c7fa2bd8
Shrinil Thakkar <shrinil.thakkar@project44.com>
2025-02-19 00:31:25 +0530

    shipment creation call working

--- src/Components/VoiceInput.js
+++ src/Components/VoiceInput.js
@@ -5,7 +5,7 @@ import SendIcon from '@mui/icons-material/Send';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI("AIzaSyAqPFUB4MdQILFmdMCOdeDD-g08GFY8sw4");
-const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-001" });
 
 const VoiceInput = () => {
     const [inputValue, setInputValue] = useState('');
@@ -22,35 +22,87 @@ const VoiceInput = () => {
         console.log('Message sent:', inputValue);
         // resetTranscript(); // Clear the transcript
 
-        const prompt = `Give me name of place from this text "${inputValue}", Find origin or start point and end point and identifier which can be either BOL,Bill of load, Booking number, Housebill or lading from this text and return me response in json format { origin, destionation, identifier }`;
-        const result = await model.generateContent(prompt, {
-            responseMimeType: 'application/json',
-        });
-        const response = await result.response;
-        const text = response.text();
-        const filteredText = text.replace(/```json\n|```/g, '');
-        const token = ``
+        const prompt = `For this put text: "${inputValue}.\n", 
+        Extract following things from the input text:
+         1. origin: origin city name (including common abbreviations like SF is San Fransisco),
+         2. destination: destination city name (including common abbreviations like SF is San Fransisco), 
+         3. identifier: identifier can be either bill of lading (can be referred as BOL), booking number (can be referred as BN) or House bill of lading (can be referred as HBOL), by default it wiill be BOL with value BOL_123
+         4. carrier_scac: carriers SCAC code is generally a 4 letter code of uppercase alphabets, if customer provides a carrier name, take first 4 letters of it, for example if carrier name is "Maersk Line", carrier_scac will be "MAER". If no carrier is provided, take "CARR" as default value. 
+        Get me 5 letter UN/LOCODE for the origin and destination city as origin_locode and destination_locode. 
+        Return me a response in following json format:
+        { 
+            identifiers: [
+                {
+                    "type": "BILL_OF_LADING" or "BOOKING_NUMBER" or "HOUSE_BILL_OF_LADING",
+                    "value": <identifier>
+                },
+                {
+                    "type": assign value as "FFW_SCAC", if identifier is HOUSE_BILL_OF_LADING, else assign value as "CARRIER_SCAC",
+                    "value": <carrier>
+                }
+            ],
+            routeInfo: {
+                "stops": [
+                    {
+                        "type": "ORIGIN",
+                        "location": {
+                            "name": <origin>,
+                            "identifiers": [
+                                {
+                                    "type": "PORT_UN_LOCODE",
+                                    "value": <origin_locode>
+                                }
+                            ],
+                            "address": {}
+                        }
+                    },
+                    {
+                        "type": "DESTINATION",
+                        "location": {
+                            "name": <destination>,
+                            "identifiers": [
+                                {
+                                    "type": "PORT_UN_LOCODE",
+                                    "value": <destination_locode>
+                                }
+                             ],
+                            "address": {}
+                        }
+                    }
+                ]
+            }
+        }`;
 
         try {
+            const result = await model.generateContent(prompt, {
+                responseMimeType: 'application/json',
+            });
+            const response = await result.response;
+            const text = await response.text();
+            console.log('Json:', text);
+            const filteredText = text.replace(/```json\n|```/g, '');
             const jsonResponse = JSON.parse(filteredText); // Ensure valid JSON
-            const request = await fetch('https://na12.api.project44.com/api/v4/shipments/tracking', {
+
+
+            const request = await fetch('http://localhost:5050/proxy/shipment', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'X-Tenant-Id': '1728281920130',
-                    'X-User-Id': '-1',
-                    'Authorization': `Bearer ${token}`,
-                    'X-Tenant-Uuid': '92ca34e7-1d92-4f65-8f02-41619996551d',
-                    // "Access-Control-Allow-Origin": "*"
                 },
                 body: JSON.stringify(jsonResponse),
             });
-            const data = await request.json();
-            console.log(data);
-          } catch (error) {
-            console.error("Invalid JSON format:", text);
-          }
-        
+
+            if (request.headers.get('Content-Type')?.includes('application/json')) {
+                const data = await request.json();
+                console.log("Data:", data);
+            } else {
+                const text = await request.text();
+                console.error("Unexpected response format:", text);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+
         setInputValue(''); // Clear the input value
     };
 
